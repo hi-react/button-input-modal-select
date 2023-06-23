33ae192c2f96ed3824cf183dae13bc2ee479766a
heeyeon <godeeyeon@gmail.com>
2023-06-23 11:39:21 +0900

    style. 컴포넌트들 import

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Layout from "./components/Layout";
+import Button from "./components/Button";
+import Input from "./components/Input";
+import Modal from "./components/Modal";
+import Select from "./components/Select";
+
+function App() {
+  return (
+    <Layout>
+      <Button />
+      <Input />
+      <Modal />
+      <Select />
+    </Layout>
+  );
+}
+
+export default App;
